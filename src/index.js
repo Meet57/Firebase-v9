2ea8a7c4d04f98948db7cc7bc2f45d18c23d58f4
meet57 <meet57.mp@gmail.com>
2022-02-01 10:20:35 +0530

    firebase setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+// v8: import firebase from 'firebase/app'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCWcWJF5VeONb-2yKyjiwEOfF-xkRZFiZc",
+    authDomain: "fir-v9-b3b1b.firebaseapp.com",
+    projectId: "fir-v9-b3b1b",
+    storageBucket: "fir-v9-b3b1b.appspot.com",
+    messagingSenderId: "548578442574",
+    appId: "1:548578442574:web:1eb67199b04c22efe3321f",
+};
+
+// v8: firebaseConfig.initializeApp(firebaseConfig)
